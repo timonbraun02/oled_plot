bd9add4ceb3f2289a3e92782fccecb341048b3f0
Timon Braun <44687080+timonbraun02@users.noreply.github.com>
2023-06-28 17:37:05 +0200

    Update Graph_test.ino

--- Graph_test.ino
+++ Graph_test.ino
@@ -1,32 +1,45 @@
 #include "Graph_v2.h"
+// library imports all of the oled libs
+// if you use a 32x128 pixel oled, you need to change the #define SCREEN_HEIGHT to 32 in "Graph_v2.h", otherwise it will try to plot outside the visable screen
 
-graph Graph1(2, 50); // create graph (num signals, num data points in graph)
-graph Graph2(1,50); // default constructor (1, 50) 1 signal, 50 data points per plot
+  /**
+   * Note:
+   * - for educational purposes
+   * - expect bugs
+   * - have fun
+   * - thanks :)
+   */
+
+graph Graph1(2, 100); // create graph (num signals -> max. 6, num data points in graph -> max. 255)
+graph Graph2; // default constructor (1, 50) 1 signal, 50 data points per plot
 
 void setup() {
 
-  init_graph_v2();
+  init_graph_v2(); // set up the library
 
   Graph1.drawAxis(15, 128, 1, 28); // init graph with dimensions (x1, x2, y1, y2)
-  Graph2.drawAxis(40, 80, 34, 62); // init graph with dimensions
+  Graph2.drawAxis(40, 80, 34, 62); // init smaller graph
 }
 
 void loop() {
   float t = micros() / 1e6;
-  int signal_1[2];
-  int signal_2[1];
+  int graph1_signal[2];
+  int graph2_signal[1];
 
-  signal_1[0] = 12 + 10 * sin(2 * PI * t * 0.05);
-  signal_1[1] = 12 + 5 * sin(2 * PI * t * 0.1);
+  //two diffent signals for Graph1
+  graph1_signal[0] = 12 + 10 * sin(2 * PI * t * 0.05);
+  graph1_signal[1] = 12 + 5 * sin(2 * PI * t * 0.1);
 
-  signal_2[0] = 10 + 10 * sin(2 * PI * t * 0.2);
+  graph2_signal[0] = 10 + 10 * sin(2 * PI * t * 0.2);
 
   display.clearDisplay();
 
-  Graph1.addDataPoint(signal_1); // create data point with val
+  // just plot the signal, not showing the avr, max and min vals
+  Graph1.addDataPoint(graph1_signal); // create data point with signal_1 ( must be int var_name[] type! )
   Graph1.drawDataLine(); // connect all data poits with line
 
-  Graph2.addDataPoint(signal_2); // create data point with val
+  // plotting data and showing the avr, max and min vals of the currently plotted data
+  Graph2.addDataPoint(graph2_signal); // create data point with signal_2 ( must be int var_name[] type! )
   Graph2.drawDataLine(); // connect all data poits with line
   display.setCursor(25, 35);
   display.print(Graph2.getAverage(0));
@@ -37,5 +50,4 @@ void loop() {
 
   display.display();
   delay(200);
-
 }
