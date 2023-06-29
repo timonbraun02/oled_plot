ae1adb3478b6f3e5a9f4e6a5e3bfbc89f9d0ec57
Timon Braun <44687080+timonbraun02@users.noreply.github.com>
2023-06-29 07:45:06 +0200

    Update and rename Graph_v2.h to Graph_v3.h
    Added the requested manual zoom feature

--- Graph_v3.h
+++ Graph_v3.h
@@ -3,12 +3,36 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+  /**
+   * Note:
+   * - Version 3
+   * - for educational purposes
+   * - expect bugs
+   * - have fun
+   * - thanks :)
+   */
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void init_graph()
+{
+  Serial.begin(9600);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+  display.display();
+  display.clearDisplay();
+  display.setTextSize(1);      // set text size
+  display.setTextColor(WHITE); // set text color
+}
 
 class graph
 {
@@ -17,14 +41,18 @@ public:
   int num_signals;
   int data_len;
   graph() : num_signals(1), data_len(50) {}
-  graph(int num, int len = 50) { num_signals = num; data_len = len;}
-
+  graph(int num, int len = 50)
+  {
+    num_signals = num;
+    data_len = len;
+  }
+  bool autoZoom = true;
   const static int max_num_signals = 6;
   const static int max_samples = 255;
   int X_startVal, X_endVal, Y_startVal, Y_endVal;
   int data[max_num_signals][max_samples];
   int Coords[max_num_signals][max_samples][2];
-  int Y_max = 0;
+  int Y_max = 0, manual_Y_max = 0;
   int Y_min = 0;
   int points[max_num_signals] = {0, 0, 0, 0, 0, 0};
   int avr[max_num_signals];
@@ -52,8 +80,10 @@ public:
 
   void addDataPoint(int val[])
   {
+
     Y_max = 0;
     Y_min = 0xFFFF;
+
     for (byte j = 0; j < num_signals; j++)
     {
       if (points[j] < data_len)
@@ -72,6 +102,7 @@ public:
       avr[j] = 0;
       for (byte i = 0; i < points[j]; i++)
       {
+
         if (data[j][i] > Y_max)
         {
           Y_max = data[j][i];
@@ -80,6 +111,7 @@ public:
         {
           Y_min = data[j][i];
         }
+
         avr[j] += data[j][i];
       }
       avr[j] = avr[j] / points[j];
@@ -105,6 +137,24 @@ public:
     return Y_min;
   }
 
+  // void drawDataPoints()
+  // {
+  //   drawAxis(X_startVal, X_endVal, Y_startVal, Y_endVal);
+  //   for (byte i = 0; i < points; i++)
+  //   {
+
+  //     int X_coord = ((((i + 1) * 100 / points) * (X_endVal - X_startVal)) / 100) + X_startVal;
+  //     int point = data[i];
+  //     int Y_coord = 0;
+  //     if (point > 0)
+  //     {
+  //       Y_coord = ((point * 100 / Y_max) * Y_endVal) / 100;
+  //     }
+  //     Y_coord = map(Y_coord, Y_startVal, Y_endVal, Y_endVal, Y_startVal);
+  //     display.drawPixel(X_coord, Y_coord, WHITE);
+  //   }
+  // }
+
   void drawDataLine()
   {
     for (byte j = 0; j < num_signals; j++)
@@ -117,7 +167,15 @@ public:
         int Y_coord = 0;
         if (point > 0)
         {
-          Y_coord = ((point * 100 / Y_max) * Y_endVal) / 100;
+          if (autoZoom)
+          {
+            Y_coord = ((point * 100 / Y_max) * Y_endVal) / 100;
+            manual_Y_max = Y_max;
+          }
+          else
+          {
+            Y_coord = ((point * 100 / manual_Y_max) * Y_endVal) / 100;
+          }
         }
         Y_coord = map(Y_coord, Y_startVal, Y_endVal, Y_endVal, Y_startVal);
         Coords[j][i][0] = X_coord;
@@ -129,4 +187,9 @@ public:
       }
     }
   }
+
+void set_Y_Zoom(int maxVal){
+  manual_Y_max = maxVal;
+}
+
 };
